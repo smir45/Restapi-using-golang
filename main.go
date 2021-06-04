5123e857bffa91ee4c84599d7d8433e0337304ca
Samir Mishra <smir.mishra1551@gmail.com>
2021-06-04 22:20:35 +0545

    adding comments

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func testAddArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test ADD endpoint is working")
 }
 
-//creating delete
+//creating delete functions
 func testDeleteArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test DELETE endpoint is working")
 }
